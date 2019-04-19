816a5055f0edfd00ab4b86f1d61882e3fcffcf41
188867052 <188867052@qq.com>
2019-04-19 17:09:42 +0800

    add  template

--- Core.Web/Login/Login.cs
+++ Core.Web/Login/Login.cs
@@ -1,9 +1,8 @@
 ﻿namespace Core.Web.Dialog
 {
-    //file:///C:/Users/54215/OneDrive/素材库/60套HTML5+CSS3后台管理登录模板/1649-60套HTML5+CSS3后台管理登录模板/045/grid.html#
     public class Login
     {
-        private string html = System.IO.File.ReadAllText(@"C:\Users\54215\Desktop\Study\Asp.Net\Core.Web\File\Login.html");
+        private string html = System.IO.File.ReadAllText(@"C:\Users\54215\Desktop\Study\Asp.Net\Core.Mvc\wwwroot\045\index.html");
 
         public Login()
         {
