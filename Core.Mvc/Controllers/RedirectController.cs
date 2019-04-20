77126702aec866a51998c821834a4e9265f73f45
188867052 <188867052@qq.com>
2019-04-20 10:31:05 +0800

    Redirect/

--- Core.Mvc/Controllers/RedirectController.cs
+++ Core.Mvc/Controllers/RedirectController.cs
@@ -12,17 +12,19 @@ namespace Core.Mvc.Controllers
         {
             this._hostingEnvironment = hostingEnvironment;
         }
+
         public IActionResult Index()
         {
             File File = new File(_hostingEnvironment, "index");
             return Content(File.Render(), "text/html");
         }
 
-        public IActionResult Dashboard()
+        public IActionResult Index2()
         {
-            File File = new File(_hostingEnvironment, "dashboard");
+            File File = new File(_hostingEnvironment, "index2");
             return Content(File.Render(), "text/html");
         }
+
 
         public IActionResult Tables()
         {
@@ -42,15 +44,101 @@ namespace Core.Mvc.Controllers
             return Content(File.Render(), "text/html");
         }
 
-        public IActionResult Grid()
+        public IActionResult Buttons()
         {
-            File File = new File(_hostingEnvironment, "Grid");
+            File File = new File(_hostingEnvironment, "buttons");
+            return Content(File.Render(), "text/html");
+        }
+
+        public IActionResult Calendar()
+        {
+            File File = new File(_hostingEnvironment, "calendar");
+            return Content(File.Render(), "text/html");
+        }
+
+        public IActionResult Charts()
+        {
+            File File = new File(_hostingEnvironment, "charts");
             return Content(File.Render(), "text/html");
         }
 
         public IActionResult Chat()
         {
             File File = new File(_hostingEnvironment, "Chat");
+            return Content(File.Render(), "text/html");
+        }
+
+        public IActionResult Dashboard()
+        {
+            File File = new File(_hostingEnvironment, "dashboard");
+            return Content(File.Render(), "text/html");
+        }
+
+        public IActionResult Error403()
+        {
+            File File = new File(_hostingEnvironment, "Error403");
+            return Content(File.Render(), "text/html");
+        }
+
+        public IActionResult Error404()
+        {
+            File File = new File(_hostingEnvironment, "Error404");
+            return Content(File.Render(), "text/html");
+        }
+
+        public IActionResult Error405()
+        {
+            File File = new File(_hostingEnvironment, "Error405");
+            return Content(File.Render(), "text/html");
+        }
+
+        public IActionResult Error500()
+        {
+            File File = new File(_hostingEnvironment, "Error500");
+            return Content(File.Render(), "text/html");
+        }
+
+        public IActionResult FormCommon()
+        {
+            File File = new File(_hostingEnvironment, "Form-Common");
+            return Content(File.Render(), "text/html");
+        }
+
+        public IActionResult Formvalidation()
+        {
+            File File = new File(_hostingEnvironment, "form-validation");
+            return Content(File.Render(), "text/html");
+        }
+
+
+        public IActionResult FormWizard()
+        {
+            File File = new File(_hostingEnvironment, "form-wizard");
+            return Content(File.Render(), "text/html");
+        }
+
+        public IActionResult Gallery()
+        {
+            File File = new File(_hostingEnvironment, "gallery");
+            return Content(File.Render(), "text/html");
+        }
+
+        public IActionResult Grid()
+        {
+            File File = new File(_hostingEnvironment, "Grid");
+            return Content(File.Render(), "text/html");
+        }
+
+   
+        public IActionResult Interface()
+        {
+            File File = new File(_hostingEnvironment, "interface");
+            return Content(File.Render(), "text/html");
+        }
+
+        public IActionResult Invoice()
+        {
+            File File = new File(_hostingEnvironment, "invoice");
             return Content(File.Render(), "text/html");
         }
     }
