c3478dc5018ae4929c8b612d6e35cae108f4bb16
188867052 <188867052@qq.com>
2019-04-21 16:45:16 +0800

    ok

---
+++ Core.Api/Controllers/Api/V1/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.Api.Extensions;
+using Core.Api.Models.Response;
+using Core.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Api.Controllers11
+{
+    /// <summary>
+    /// 用户控制器
+    /// </summary>
+    //[CustomAuthorize]
+    [Route("/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly Context _dbContext;
+        public UserController(Context dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            using (this._dbContext)
+            {
+                List<User> list = this._dbContext.User.ToList();
+                ResponseModel response = ResponseModelFactory.CreateInstance;
+                response.SetData(list);
+                return Ok(response);
+            }
+        }
+    }
+}
