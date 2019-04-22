2971ba2d0c1e3c09f776e771ddd90eda532cfcfa
188867052 <188867052@qq.com>
2019-04-22 20:52:05 +0800

    add

--- Core.Web/Grid/BaseGridColumn.cs
+++ Core.Web/Grid/BaseGridColumn.cs
@@ -13,13 +13,12 @@
         {
             return $"<th>{this.Thead}</th>"; ;
         }
-        public abstract string RenderTd(T entity);
 
+        public abstract string RenderTd(T entity);
 
         protected virtual string RenderTd(object value)
         {
             return $"<td>{value}</td>";
         }
-
     }
 }
--- Core.Web/Grid/EnumColumn.cs
+++ Core.Web/Grid/EnumColumn.cs
@@ -15,7 +15,7 @@ namespace Core.Web.Grid
         public override string RenderTd(T entity)
         {
             var value = this.expression.Compile()(entity);
-            return $"<td><li><i class=\"{value}\"></i></li></td>";
+            return base.RenderTd(value);
         }
     }
 }
