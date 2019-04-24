c5f5ae997b94bf512089033cdb0b4d05d1815dec
188867052 <188867052@qq.com>
2019-04-24 22:53:42 +0800

    add

---
+++ Core.Mvc/wwwroot/js/Log/index.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+
+    window.Index = function () {
+    };
+
+    window.Index.prototype = {
+
+        // Private Fields
+
+        // Public Properties
+
+        // Public Methods
+
+
+        search: function () {
+            alert();
+
+
+        }
+
+        // Private Methods
+    };
+})();
