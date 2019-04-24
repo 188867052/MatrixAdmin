ee048ea43e826c692cd299821769fab69cfeee30
188867052 <188867052@qq.com>
2019-04-24 10:41:47 +0800

    add

--- Core.Web/GridFilter/BaseGridFilter.cs
+++ Core.Web/GridFilter/BaseGridFilter.cs
@@ -12,9 +12,9 @@
         public virtual string Render()
         {
             return $"<div class=\"custom-control-inline\">" +
-                       $"<label class=\"custom-control-label\">{this.Value}</label>" +
-                       $"<input class=\"form-control\" type=\"text\">" +
-                       $"</div>";
+                      $"<label>{this.Value}</label>" +
+                      $"<input type=\"text\">" +
+                   $"</div>";
         }
     }
 }
