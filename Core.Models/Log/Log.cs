1064ed4f8a4deaba022dd090f546cb756cb16417
188867052 <188867052@qq.com>
2019-04-29 15:01:13 +0800

    add

---
+++ Core.Models/Log/Log.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Core.Model.Log
+{
+    /// <summary>
+    /// 日志实体类
+    /// </summary>
+    public class Log
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+    }
+}
