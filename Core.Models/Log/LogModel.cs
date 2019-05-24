5a54eda1c05a5e420687a4beb548dc671f05841b
188867052 <188867052@qq.com>
2019-05-24 20:47:17 +0800

    jenkins

--- Core.Models/Log/LogModel.cs
+++ Core.Models/Log/LogModel.cs
@@ -17,7 +17,7 @@ namespace Core.Model.Log
             this.LogLevel = (LogLevel?)entity.LogLevel;
             this.CreateTime = entity.CreateTime;
             this.SqlType = (SqlTypeEnum)entity.SqlOperateType;
-            int a = 666;
+            int a = 6626;
         }
 
         /// <summary>
