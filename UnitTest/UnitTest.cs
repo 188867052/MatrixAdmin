4876ca1e1532f5e0e6cb03daaf07b2a5cdb10040
188867052 <188867052@qq.com>
2019-05-25 13:04:06 +0800

    Add MSTest to jenkins

---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
