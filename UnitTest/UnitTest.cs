38f754dadc45c30eeccf02395bdd36b0d4b7ae26
188867052 <188867052@qq.com>
2019-05-25 15:57:22 +0800

    change Unit Test success, the MSTest Plugin in jenkins seems not work, and MSTest cmd is not support dot net core.

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -3,22 +3,20 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTest
 {
-    [TestClass]
-    [TestCategory("MyUnitTest")]
+    [TestClass, TestCategory("MyUnitTest")]
     public class UnitTest
     {
         [TestMethod]
         [TestCategory("MyUnitTest")]
         public void TestMethod()
         {
-            Assert.IsTrue(false);
+            Assert.IsTrue(true);
         }
 
         [TestMethod]
         [TestCategory("MyUnitTest")]
         public void AddNumberTest()
         {
-            //正确数据，最好是旧版本已经过校验的数据
             int i = 5, j = 6;
             int result = 11;
 
