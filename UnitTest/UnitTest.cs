afd44bad36fef6ead5da50848bcc6eee802b5bcb
188867052 <188867052@qq.com>
2019-05-25 16:23:39 +0800

    UT massage

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -11,17 +11,17 @@ namespace UnitTest
         public void TestMethod()
         {
             Console.WriteLine("this is a test message.");
-            Assert.IsTrue(true,"test success!");
+            Assert.IsTrue(true, "test success!");
         }
-        
+
         [TestMethod]
         [TestCategory("MyUnitTest")]
         public void AddNumberTest()
         {
             int i = 5, j = 6;
             int result = 11;
-
-            Assert.AreEqual(result, i + j);
+            Console.WriteLine("this is a test message2.");
+            Assert.AreNotEqual(result, i + j,"test fail");
         }
     }
 }
