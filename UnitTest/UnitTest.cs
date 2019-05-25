68fb22a9a7d76cb0b8f85ed512fa03a0814dacf6
188867052 <188867052@qq.com>
2019-05-25 17:24:34 +0800

    TestDataBaseConnection

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -1,4 +1,5 @@
-using System;
+using Core.Entity;
+using Dapper;
 using NUnit.Framework;
 
 namespace UnitTest
@@ -7,10 +8,11 @@ namespace UnitTest
     public class UnitTest
     {
         [Test]
-        public void TestMethod()
+        public void TestDataBaseConnection()
         {
-            Console.WriteLine("test fail 1");
-            Assert.IsTrue(false, "test fail!");
+            string sql = $"UPDATE [User] SET IsDeleted = @IsDeleted WHERE IsDeleted = @IsDeleted";
+            CoreApiContext.Dapper.Execute(sql, new { IsDeleted = false});
+            Assert.IsTrue(true,"数据库连接正常");
         }
 
         [Test]
@@ -18,7 +20,6 @@ namespace UnitTest
         {
             int i = 5, j = 6;
             int result = 11;
-            Console.WriteLine("test fail 2");
             Assert.AreNotEqual(result, i + j,"test fail");
         }
     }
