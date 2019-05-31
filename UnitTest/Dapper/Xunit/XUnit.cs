31d396a6ed66c0657adfab902f01b6e293d38c95
188867052 <188867052@qq.com>
2019-05-31 10:38:26 +0800

    add XUnit

---
+++ UnitTest/Dapper/Xunit/XUnit.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Core.UnitTest.XUnit
+{
+    /// <summary>
+    /// Api unit test.
+    /// </summary>
+    public class XUnit
+    {
+        [Fact]
+        public void Add()
+        {
+            Assert.Equal(3, 3);
+        }
+    }
+}
