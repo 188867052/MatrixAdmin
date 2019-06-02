5e2a1e702a82e960f3d3d18e4bf3179abbe5eff0
188867052 <188867052@qq.com>
2019-06-02 23:11:36 +0800

    update

--- Core.Extension/Dapper/TableInfo.cs
+++ Core.Extension/Dapper/TableInfo.cs
@@ -1,22 +1,5 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using Core.Entity;
-using Dapper;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
-
-namespace Core.Extension.Dapper
+﻿namespace Core.Extension.Dapper
 {
-
     public class TableInfo
     {
         public string TableName { get; set; }
