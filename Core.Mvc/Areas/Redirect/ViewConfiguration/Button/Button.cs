047ad603d338763b5a0770b52ce74a98c982b488
188867052 <188867052@qq.com>
2019-06-03 11:51:03 +0800

    避免有问题的同步等待

--- Core.Mvc/Areas/Redirect/ViewConfiguration/Button/Button.cs
+++ Core.Mvc/Areas/Redirect/ViewConfiguration/Button/Button.cs
@@ -8,6 +8,7 @@ using Core.Mvc.Areas.Redirect.ViewConfiguration.Home;
 using Core.Mvc.Framework;
 using Core.Web.Sidebar;
 using Core.Web.ViewConfiguration;
+using Microsoft.VisualStudio.Threading;
 
 namespace Core.Mvc.Areas.Redirect.ViewConfiguration.Button
 {
@@ -19,7 +20,9 @@ namespace Core.Mvc.Areas.Redirect.ViewConfiguration.Button
         {
             var url = new Url(typeof(Api.Controllers.IconController), nameof(Api.Controllers.IconController.Index));
             Task<ResponseModel> a = HttpClientAsync.GetAsync<IList<Icon>>(url);
+#pragma warning disable VSTHRD002 // 避免有问题的同步等待
             List<Icon> icons = (List<Icon>)a.Result.Data;
+#pragma warning restore VSTHRD002 // 避免有问题的同步等待
             string iconHtml = default;
             foreach (var icon in icons)
             {
