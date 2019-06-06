6fac3c4026714a06049df887907c48d5381e8076
188867052 <188867052@qq.com>
2019-06-06 15:56:10 +0800

    MSBuild: Logger

---
+++ Core.Logger/MyLogger.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+
+namespace Core.MyLogger
+{
+    public class MyLogger : Logger
+    {
+        public override void Initialize(IEventSource eventSource)
+        {
+            //Register for the ProjectStarted, TargetStarted, and ProjectFinished events
+            eventSource.ProjectStarted += new ProjectStartedEventHandler(eventSource_ProjectStarted);
+            eventSource.TargetStarted += new TargetStartedEventHandler(eventSource_TargetStarted);
+            eventSource.ProjectFinished += new ProjectFinishedEventHandler(eventSource_ProjectFinished);
+        }
+
+        void eventSource_ProjectStarted(object sender, ProjectStartedEventArgs e)
+        {
+            Console.WriteLine("Project Started: " + e.ProjectFile);
+        }
+
+        void eventSource_ProjectFinished(object sender, ProjectFinishedEventArgs e)
+        {
+            Console.WriteLine("Project Finished: " + e.ProjectFile);
+        }
+
+        void eventSource_TargetStarted(object sender, TargetStartedEventArgs e)
+        {
+            if (Verbosity == LoggerVerbosity.Detailed)
+            {
+                Console.WriteLine("Target Started: " + e.TargetName);
+            }
+        }
+    }
+}
