37bd4b19b6cbe8c53c6688f536285b40a31ac238
188867052 <188867052@qq.com>
2019-06-11 20:19:14 +0800

    cross-origin resource sharing

--- Core.Api/Configurations/CorsConfiguration.cs
+++ Core.Api/Configurations/CorsConfiguration.cs
@@ -10,19 +10,24 @@ namespace Core.Api.Configurations
     {
         public static void AddService(IServiceCollection services)
         {
-            services.AddCors(o =>
-               o.AddPolicy("*",
-                   builder => builder
-                       .AllowAnyHeader()
-                       .AllowAnyMethod()
-                       .AllowAnyOrigin()
-                       .AllowCredentials()
-               ));
+            string[] urls = { "http://localhost:90" };
+            services.AddCors(options =>
+            options.AddPolicy("AllowSameDomain", builder => builder.WithOrigins(urls).AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin().AllowCredentials()));
+
+            //services.AddCors(o =>
+            //   o.AddPolicy("*",
+            //       builder => builder
+            //           .AllowAnyHeader()
+            //           .AllowAnyMethod()
+            //           .AllowAnyOrigin()
+            //           .AllowCredentials()
+            //   ));
         }
 
         public static void AddConfigure(IApplicationBuilder app)
         {
-            app.UseCors("*");
+            app.UseCors("AllowSameDomain");
+            //app.UseCors("*");
         }
     }
 }
