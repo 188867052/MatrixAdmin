a0638a36dd868af708e492ebbeea8da1978cf30a
188867052 <188867052@qq.com>
2019-06-11 20:45:47 +0800

    Fix Cors issues

--- Core.Api/Configurations/CorsConfiguration.cs
+++ Core.Api/Configurations/CorsConfiguration.cs
@@ -10,24 +10,19 @@ namespace Core.Api.Configurations
     {
         public static void AddService(IServiceCollection services)
         {
-            string[] urls = { "http://localhost:90" };
-            services.AddCors(options =>
-            options.AddPolicy("AllowSameDomain", builder => builder.WithOrigins(urls).AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin().AllowCredentials()));
-
-            //services.AddCors(o =>
-            //   o.AddPolicy("*",
-            //       builder => builder
-            //           .AllowAnyHeader()
-            //           .AllowAnyMethod()
-            //           .AllowAnyOrigin()
-            //           .AllowCredentials()
-            //   ));
+            services.AddCors(o =>
+               o.AddPolicy("*",
+                   builder => builder
+                       .AllowAnyHeader()
+                       .AllowAnyMethod()
+                       .AllowAnyOrigin()
+                       .AllowCredentials()
+               ));
         }
 
         public static void AddConfigure(IApplicationBuilder app)
         {
-            app.UseCors("AllowSameDomain");
-            //app.UseCors("*");
+            app.UseCors("*");
         }
     }
 }
